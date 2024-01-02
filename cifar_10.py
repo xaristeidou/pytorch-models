f9b14156bde3271efb0480906faea6c0b4a84f61
Christoforos Aristeidou <aristeidouxristos@gmail.com>
2024-01-02 19:42:16 +0200

    added basic transforms

--- cifar_10.py
+++ cifar_10.py
@@ -3,3 +3,10 @@ import torchvision.datasets as datasets
 import torchvision.transforms as transforms
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+transform = transforms.Compose([
+    transforms.RandomHorizontalFlip(),
+    transforms.RandomCrop(32, padding = 4),
+    transforms.ToTensor(),
+    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+])
