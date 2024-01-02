bade661fc3adbeb1255d75dd84ff2ec221c08fe3
Christoforos Aristeidou <aristeidouxristos@gmail.com>
2024-01-02 19:48:57 +0200

    added train dataset loading from torchvision CIFAR10

--- image_classification/cifar_10.py
+++ image_classification/cifar_10.py
@@ -10,3 +10,10 @@ transform = transforms.Compose([
     transforms.ToTensor(),
     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
 ])
+
+train_dataset = datasets.CIFAR10(
+    root = "./data",
+    train = True,
+    transform = transform,
+    download = False
+)
