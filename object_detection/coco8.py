169bee58510c5fe415129066665c7d1bcefb3a7a
Christoforos Aristeidou <aristeidouxristos@gmail.com>
2024-01-10 23:55:51 +0200

    added class object for detector with resnet backbone

--- object_detection/coco8.py
+++ object_detection/coco8.py
@@ -1,9 +1,16 @@
 import torch
 import torch.nn as nn
+import torchvision
 import torchvision.datasets as datasets
+
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 train_dataset = datasets.CocoDetection() # TODO download dataset
 test_dataset = datasets.CocoDetection() # TODO download dataset
 
+class ObjectDetector(nn.Module):
+    def __init__(self, num_classes=2):
+        super(ObjectDetector, self).__init__()
+
+        self.backbone = torchvision.models.resnet50(pretrained=True)
